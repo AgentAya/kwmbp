a6efa3b91ddd0979b570412c672ab63e1389581b
AgentAya <etudaiye.abdulmumuni@federalpolyede.edu.ng>
2025-10-16 23:46:46 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
- <?php
+<?php
+session_start();
  require_once __DIR__ . '/../config/database.php';
  require_once __DIR__ . '/../routes/web.php';
-
-session_start();
-
 // Redirect to welcome page
 header("Location: welcome.php");
 exit;
